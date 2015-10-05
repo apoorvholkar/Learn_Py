e29e548ee4141b166933286d274af1a66a7d82ae
apoorvholkar <apoorvholkar007@gmail.com>
2015-10-05 18:01:42 +0800

    Check Missing files between 2 folders

---
+++ Learn_Py/FindMismatchSegments.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import pandas as pd
+
+econList = range(110)
+
+for econID in econList:
+    try:
+        filePath1 = 'E:\\ParallelU4_Oct\\ProductionData\\DTD\\step1\\' + str(econID) 
+        filePath2 = 'E:\\ParallelU4_Oct\\ProductionData\\DTD\\step2\\' + str(econID)
+        #filePath1 = 'X:\\ProductionData\\DTD\\step1\\' + str(econID) 
+        #filePath2 = 'X:\\ProductionData\\DTD\\step2\\' + str(econID)
+        
+        
+        l1 = [int(item[-6:]) for item in os.listdir(filePath1)]
+        
+        l2 = [int(item[-6:]) for item in os.listdir(filePath2)]
+        
+        a = []
+        for item in l1:
+            if item not in l2:
+                a.append(item)
+        
+        if a != []:
+            a = pd.Series(np.asarray(a))
+            fileName = 'Econ' + str(econID) + '.csv'
+            a.to_csv(fileName)
+    except:
+        print 'no Econ' + str(econID)
